2e7f7b01b1ad2ddebfb078fcb2a51dd6d725cb12
Unchul <unchul87@gmail.com>
2025-07-27 00:14:45 +0900

    init:[임운철]우선remoteEntry까지는 불러와짐
    localhost/_nuxt/remoteEntry로 엔트리 값이 들어오고는 있는중

---
+++ aview-nuxt-frontend-main/main-container/rspack.config.ts
@@ -0,0 +1,103 @@
+import * as path from "node:path";
+import { defineConfig } from "@rspack/cli";
+import { rspack } from "@rspack/core";
+import * as RefreshPlugin from "@rspack/plugin-react-refresh";
+import { ModuleFederationPlugin } from "@module-federation/enhanced/rspack";
+
+import { mfConfig } from "./module-federation.config";
+
+const isDev = process.env.NODE_ENV === "development";
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"];
+
+export default defineConfig({
+  context: __dirname,
+  entry: {
+    main: "./src/index.ts",
+  },
+  resolve: {
+    extensions: ["...", ".ts", ".tsx", ".jsx"],
+  },
+
+  devServer: {
+    port: 80,
+    historyApiFallback: true,
+    proxy: [
+      {
+        // /nuxt/** 요청을 localhost:3000 으로 프록시
+        context: ["/nuxt"],
+        target: "http://localhost:3000",
+        pathRewrite: { "^/nuxt": "" },
+        changeOrigin: true,
+      },
+    ],
+  },
+  output: {
+    // You need to set a unique value that is not equal to other applications
+    uniqueName: "main_container",
+    // publicPath must be configured if using manifest
+    publicPath: "http://localhost:80/",
+  },
+
+  experiments: {
+    css: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+      {
+        test: /\.css$/,
+        use: ["postcss-loader"],
+        type: "css",
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        use: [
+          {
+            loader: "builtin:swc-loader",
+            options: {
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: "automatic",
+                    development: isDev,
+                    refresh: isDev,
+                  },
+                },
+              },
+              env: { targets },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: "./index.html",
+      scriptLoading: "module",
+    }),
+    new ModuleFederationPlugin({
+      ...mfConfig,
+      remoteType: "script",
+    }),
+    isDev ? new RefreshPlugin() : null,
+  ].filter(Boolean),
+  optimization: {
+    minimizer: [
+      new rspack.SwcJsMinimizerRspackPlugin(),
+      new rspack.LightningCssMinimizerRspackPlugin({
+        minimizerOptions: { targets },
+      }),
+    ],
+  },
+});
